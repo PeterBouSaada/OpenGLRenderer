a1c00d4fb4b21fb46ee497c791a733db2533ae0f
peter bou saada <peterbousaada@gmail.com>
2022-01-03 23:09:57 -0500

    Added a simple window.

--- src/entry.cpp
+++ src/entry.cpp
@@ -4,5 +4,24 @@
 #include <glm/glm.hpp>
 
 int main() {
-    return 0;
+
+	glfwInit();
+
+	glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Test window", nullptr, nullptr);
+	glfwMakeContextCurrent(window);
+	glfwSetWindowCloseCallback(window, [](GLFWwindow* window){ glfwSetWindowShouldClose(window, true); });
+	while(!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+
+	return 0;
 }
